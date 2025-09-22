4bacb9115744c6f88ef996e0b1096423d2fb460f
백승한 <shbaik0607@gmail.com>
2025-09-22 15:38:30 +0900

    문제1

--- 문제1/main.cpp
+++ 문제1/main.cpp
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 int main() {
+	const char* title = "Basic Constants";
     const char* name = "Sungwook";
     char initial = 'S';
     int age = 28;
     float score = 93.5f;
     unsigned int count = 3000000000U;
 
-	printf(" === Basic Constants === \n\n");
-	printf("Name: %s\n\n", name);
-    printf("Initial: %c\n\n", initial);
-    printf("Age: %d\n\n", age);
-    printf("Score: %.2f\n\n", score);
-    printf("Count: %u\n\n", count);
+	printf(" === %s === \n\n",title);
+	printf("Name\t: %s\n\n", name);
+    printf("Initial\t: %c\n\n", initial);
+    printf("Age : %d\n\n", age);
+    printf("Score\t: %.2f\n\n", score);
+    printf("Count\t: %u\n\n", count);
 	return 0;
 }
