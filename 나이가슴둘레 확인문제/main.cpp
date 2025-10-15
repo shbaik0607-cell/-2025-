54e88d1b783e4a7bdb78922734289deca00d5356
백승한 <shbaik0607@gmail.com>
2025-10-15 09:40:14 +0900

    나이가슴둘레

---
+++ 나이가슴둘레 확인문제/main.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(void)
+{
+	int age = 25, chest = 95;
+	char size;
+	if (age < 20)
+	{
+		if (chest < 85){
+			size = 'S';
+		}
+		else if (chest < 95){
+			size = 'M';
+		}
+		else if (chest >= 95){
+			size = 'L';
+		}
+	}
+	else 
+	{
+		if (chest < 90){
+			size = 'S';
+		}
+		else if (chest < 100){
+			size = 'M';
+		}
+		else if (chest >= 100){
+			size = 'L';
+		}
+	}
+	printf("사이즈는 %c입니다.\n", size);
+	return 0;
+
+}
