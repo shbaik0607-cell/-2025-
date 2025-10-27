792d5b2f80b134e3234f8b60abec7a8b63cc57b2
백승한 <shbaik0607@gmail.com>
2025-10-27 19:07:47 +0900

    확인문제3 (수정)

--- 확인문제 3/main.cpp
+++ 확인문제 3/main.cpp
@@ -1,25 +1,31 @@
 #include <stdio.h>
 
-int main() {
-    int i, j;
-    int size = 5; // 격자의 크기 (0부터 4까지 총 5줄)
+// 함수 선언
+double centi_to_meter(int cm);
 
-    // 바깥쪽 반복문: 행(i)을 제어 (0부터 size-1까지)
-    for (i = 0; i < size; i++) {
-        // 안쪽 반복문: 열(j)을 제어 (0부터 size-1까지)
-        for (j = 0; j < size; j++) {
-            // 대각선 조건을 확인
-            // 조건 1: i와 j의 값이 같은 경우 (i == j)
-            // 조건 2: i와 j의 합이 size-1인 경우 (i + j == size - 1)
-            if (i == j || i + j == size - 1) {
-                printf("*"); // 조건에 맞으면 아스터리스크 출력
-            }
-            else {
-                printf(" "); // 조건에 맞지 않으면 공백 출력
-            }
-        }
-        printf("\n"); // 한 행의 출력이 끝나면 줄바꿈
-    }
+int main(void)
+{
+    // 함수의 반환값을 저장할 변수
+    double res;
+
+    // 함수 호출, 반환값을 res에 저장
+    res = centi_to_meter(187);
+
+    // 반환된 res의 값 출력
+    printf("%.2lfm\n", res);
 
     return 0;
+}
+
+// 함수 정의의 시작
+double centi_to_meter(int cm)
+{
+    // 필요한 변수 선언
+    double result;
+
+    // 매개변수 cm의 값을 m단위로 환산
+    result = (double)cm / 100.0;
+
+    // 환산된 값 반환
+    return result;
 }
